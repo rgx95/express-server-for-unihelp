fad2fb84e6e9395dc628f61eb5eab009251271e8
Luca Ruggirello <luca.ruggirello95@gmail.com>
2021-05-04 17:09:45 +0200

    express server add morgan through npm install

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 const express = require('express')
-// Instantiate the app here
+const morgan = require('morgan')
 const app = express()
 
 const PORT = process.env.PORT || 4001;
 
 
+app.use(morgan('dev'));
 app.use(express.static('public'));
 
 
 app.get('/universita', function(req, res, next) {
-  console.log('get request /universita received')
-
-  /*let arr = new Array(15)
-  arr.fill('Università di ')*/
   let arr = ['Università di Pisa', 'Universita\' di Firenze', 'Universita\' di Siena', 'Universita\' di Roma', 'Universita\' di Milano', 'Universita\' di Bologna', 'Universita\' di Torino', 'Universita\' di Napoli', 'Universita\' di Palermo']
 
   res.json(arr)
 })
 
 app.get('/corsi', function(req, res, next) {
-  console.log('get request /corsi received')
 
   let arr = new Array(15)
   arr.fill('Corso ')
@@ -29,7 +25,6 @@ app.get('/corsi', function(req, res, next) {
 })
 
 app.get('/esami', function(req, res, next) {
-  console.log('get request /esami received')
 
   let arr = new Array(15)
   arr.fill('Esame ')
@@ -39,7 +34,6 @@ app.get('/esami', function(req, res, next) {
 })
 
 app.get('/login', function(req, res, next) {
-  console.log('try login with user: '+req.query.user+' & pass: '+req.query.pass)
   res.send()
 })
 
